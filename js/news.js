0e3bf1d755e8d7b6da586db01c87d71a6ece7ac9
Poland-links <115687634+july853898@users.noreply.github.com>
2022-12-22 11:48:26 +0800

    0.0

---
+++ js/news.js
@@ -0,0 +1,14 @@
+const swipers = new Swiper(".mySwipers-new", {
+    slidesPerView: 3,
+    slidesPerGroup: 3,
+    loop: true,
+    loopFillGroupWithBlank: true,
+    pagination: {
+        el: ".swiper-pagination",
+        clickable: true,
+    },
+    navigation: {
+        nextEl: ".swiper-button-next",
+        prevEl: ".swiper-button-prev",
+    },
+    });
